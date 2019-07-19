034bf9318e795db523fc47469395d185c3fbf7f5
Muhammad Aaqib <muhammadaqibrasheed@gmail.com>
2019-07-20 04:28:06 +0500

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io();
 messageContainer = document.getElementById("messages-container");
 sendMessageBtn = document.getElementById("send-message");
 message = document.getElementById("message");
--- server.js
+++ server.js
@@ -1,8 +1,48 @@
-const app = require("socket.io")(process.env.PORT || 3000)
+const PORT = process.env.PORT || 3000;
+const app = require("socket.io")(PORT)
+// const http = require("http");
+// const path = require("path");
+// const fs = require("fs");
 const users = {}
-app.use(__dirname);
 
-
+// http.createServer((req,res) => {
+//     console.log(req.url);
+//     contentType = "text/html";
+//     file = path.join(__dirname,req.url === "/" ? "index.html" : req.url); 
+//     ext = path.extname(req.url);
+//     switch(ext){
+//         case '.html': {
+//             contentType = "text/html";
+//             break;
+//         }
+//         case '.css': {
+//             contentType = "text/css";
+//             break;
+//         }
+//         case '.js': {
+//             contentType = "text/javascript";
+//             break;
+//         }
+//         case '.png': {
+//             contentType = "image/png";
+//             break;
+//         }
+//         case '.jpg': {
+//             contentType = "image/jpg";
+//             break;
+//         }
+//         case '.json': {
+//             contentType = "application/json";
+//             break;
+//         }
+//     }
+//     fs.readFile(file,(err,content)=>{
+//         if(err) throw err;
+//         res.writeHead(200,{'Content-Type':contentType});
+//         res.end(content,"utf8");
+//     });
+// }).listen(()=>{console.log("Server Running...")});
+app.use("/");
 app.on("connection",socket=>{
     console.log("Connection made by socket id: "+socket.id);
     socket.on("send-msg",data=>{
