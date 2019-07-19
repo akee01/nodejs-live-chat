2388583a5541ada0ce6d32037dcfbdb594646a03
Muhammad Aaqib <muhammadaqibrasheed@gmail.com>
2019-07-20 03:47:50 +0500

    Updates

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 const app = require("socket.io")(process.env.PORT || 3000)
-const https = require("https");
-const fs = require("fs");
 const users = {}
+app.use(__dirname);
 
-https.createServer((req,res) => {
-    fs.readFile("index.html",(err,content)=>{
-        if(err) throw err;
-        res.write(content);
-        res.writeHead(200,{'Content-Type':"text/html"});
-        res.end();
-    });
-});
 
 app.on("connection",socket=>{
     console.log("Connection made by socket id: "+socket.id);
