580c94d63f4351ecbf3793a8aa569364eb09d78f
Muhammad Aaqib <muhammadaqibrasheed@gmail.com>
2019-07-20 09:30:18 +0500

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('https://murmuring-springs-61373.herokuapp.com:20852');
 messageContainer = document.getElementById("messages-container");
 sendMessageBtn = document.getElementById("send-message");
 message = document.getElementById("message");
--- server.js
+++ server.js
@@ -4,44 +4,44 @@ const https = require("https");
 const path = require("path");
 const fs = require("fs");
 const users = {}
-console.log(PORT);
-// http.createServer((req,res) => {
-//     console.log(req.url);
-//     contentType = "text/html";
-//     file = path.join(__dirname,req.url === "/" ? "index.html" : req.url); 
-//     ext = path.extname(req.url);
-//     switch(ext){
-//         case '.html': {
-//             contentType = "text/html";
-//             break;
-//         }
-//         case '.css': {
-//             contentType = "text/css";
-//             break;
-//         }
-//         case '.js': {
-//             contentType = "text/javascript";
-//             break;
-//         }
-//         case '.png': {
-//             contentType = "image/png";
-//             break;
-//         }
-//         case '.jpg': {
-//             contentType = "image/jpg";
-//             break;
-//         }
-//         case '.json': {
-//             contentType = "application/json";
-//             break;
-//         }
-//     }
-//     fs.readFile(file,(err,content)=>{
-//         if(err) throw err;
-//         res.writeHead(200,{'Content-Type':contentType});
-//         res.end(content,"utf8");
-//     });
-// }).listen(5000,()=>{console.log("Server Running...")});
+console.log("Using Port :"+ PORT);
+http.createServer((req,res) => {
+    console.log(req.url);
+    contentType = "text/html";
+    file = path.join(__dirname,req.url === "/" ? "index.html" : req.url); 
+    ext = path.extname(req.url);
+    switch(ext){
+        case '.html': {
+            contentType = "text/html";
+            break;
+        }
+        case '.css': {
+            contentType = "text/css";
+            break;
+        }
+        case '.js': {
+            contentType = "text/javascript";
+            break;
+        }
+        case '.png': {
+            contentType = "image/png";
+            break;
+        }
+        case '.jpg': {
+            contentType = "image/jpg";
+            break;
+        }
+        case '.json': {
+            contentType = "application/json";
+            break;
+        }
+    }
+    fs.readFile(file,(err,content)=>{
+        if(err) throw err;
+        res.writeHead(200,{'Content-Type':contentType});
+        res.end(content,"utf8");
+    });
+}).listen(80,()=>{console.log("Server Running...")});
 
 app.on("connection",socket=>{
     console.log("Connection made by socket id: "+socket.id);
